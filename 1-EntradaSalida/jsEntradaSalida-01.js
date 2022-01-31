8c6211a38fb1e4e51146638d15f6ce0fa835b436
AlanM <neopirokiller@gmail.com>
2022-01-31 19:47:34 -0300

    cambio de firma

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,4 +1,6 @@
 /*
+Magariños Alan DIV I 
+E/S 01
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,4 +1,6 @@
 /*
+Magariños Alan DIV I 
+E/S 02
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
