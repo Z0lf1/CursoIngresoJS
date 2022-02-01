926cf743ba5e03bfa9aaa36ca8ada382e24a3861
AlanM <neopirokiller@gmail.com>
2022-01-31 22:44:39 -0300

    E/s 05

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -1,8 +1,16 @@
 /*
+Magariños Alan DIV I 
+E/S 05
+
 Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	alert("ok");
+	var nombreDelUSer;
+	var edadDelUser;
+	nombreDelUSer = txtIdNombre.value;
+	edadDelUser = txtIdEdad.value;
+
+	alert("Usted se llama "+ nombreDelUSer + " y tiene "+ edadDelUser +" años");
 }
 
