c1c689e225abe941a39e87b63a3fca022c0d4e92
AlanM <neopirokiller@gmail.com>
2022-01-31 22:03:38 -0300

    E/S 04

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,16 @@
-/* 
+/*
+Magariños Alan DIV I 
+E/S 04
+
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
-}
+	var nombreDelUser;
 
+	nombreDelUser = prompt("ingrese su nombre");
+
+	document.getElementById('txtIdNombre').value = nombreDelUser;
+	
+}
