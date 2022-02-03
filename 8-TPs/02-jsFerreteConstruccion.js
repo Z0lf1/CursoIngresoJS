185e22b4f54e19f2479f5db884de285af5113d9b
AlanM <neopirokiller@gmail.com>
2022-02-03 20:18:34 -0300

    correccion C de 2

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -10,23 +10,58 @@ function Rectangulo ()
 {
     var valorUno;
 	var valorDos;
-		
+	var perimetro;
+    var mensaje;
+    var requerimiento;
+
 	valorUno = txtIdLargo.value;
 	valorDos = txtIdAncho.value;
-    
     valorUno = parseInt(valorUno);
     valorDos = parseInt(valorDos);
-   
-    alert("La longitud necesaria de material es "+(valorUno+valorDos)*6);
+    perimetro= parseInt(perimetro);
+    perimetro= ((valorUno + valorDos)*2);
+    requerimiento = perimetro*3;
+    mensaje = "La longitud necesaria de material es ";
+    alert(mensaje+requerimiento);
+}
+/* Magariños Alan DIV I 
+TP 2 Ferrete Construccion
+2.	Para el departamento de Construcción:
+A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
+B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
+C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de cal, debemos mostrar cuantas bolsas se necesitan de cada uno para las medidas que nos ingresen.
+*/
+
+function Rectangulo () 
+{
+    var valorUno;
+	var valorDos;
+	var perimetro;
+    var mensaje;
+    var requerimiento;
+
+	valorUno = txtIdLargo.value;
+	valorDos = txtIdAncho.value;
+    valorUno = parseInt(valorUno);
+    valorDos = parseInt(valorDos);
+    perimetro= parseInt(perimetro);
+    perimetro= ((valorUno + valorDos)*2);
+    requerimiento = perimetro*3;
+    mensaje = "La longitud necesaria de material es ";
+    alert(mensaje+requerimiento);
 }
 function Circulo () 
 {
 	var radio;
 	var perimetroCircunferencia;
+	var requerimiento;
+	var mensaje;
     radio = txtIdRadio.value;
     radio = parseInt(radio);
+    mensaje ="La longitud necesaria de material es ";
     perimetroCircunferencia = (2*(Math.PI)*(radio));
-    alert("La longitud necesaria de material es "+(perimetroCircunferencia)*3);
+    requerimiento = perimetroCircunferencia*3;
+    alert( mensaje + requerimiento);
 }
 function Materiales () 
 {
@@ -35,6 +70,7 @@ function Materiales ()
 	var area;	
 	var bolsaCementoNecesaria;
 	var bolsaCalNecesaria;
+	var mensajeA;
 	valorUno = txtIdLargo.value;
 	valorDos = txtIdAncho.value;
     
@@ -46,8 +82,8 @@ function Materiales ()
     bolsaCal = parseInt(bolsaCalNecesaria);
     bolsaCementoNecesaria = area * 2;
     bolsaCalNecesaria = area * 3;
-
-
-
-    alert("La cantidad necesaria de materiales para cubrir el area es "+bolsaCementoNecesaria+" bolsas de cemento y "+ bolsaCalNecesaria+" bolsas de cal ");
+    mensajeA = " La cantidad necesaria de materiales para cubrir el area es ";
+    mensajeA += bolsaCementoNecesaria + " bolsas de cemento y "
+    mensajeA += bolsaCalNecesaria+" bolsas de cal" 
+    alert(mensajeA);
 }
