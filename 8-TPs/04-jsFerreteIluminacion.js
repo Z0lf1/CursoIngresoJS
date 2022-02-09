b8a1223037cda37c107b96baa3e43f67cdccf496
AlanM <neopirokiller@gmail.com>
2022-02-09 04:39:57 -0300

    Lamparitas

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -1,14 +1,88 @@
 /*4.	Para el departamento de iluminación:
 Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
 A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
-B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
-C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
-D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
-E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
+B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % 
+y si es de otra marca el descuento es del 30%.
+C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un
+ descuento del 25 % y si es de otra marca el descuento es del 20%.
+D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%,
+ si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
+E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos 
+brutos en informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
 
  */
 function CalcularPrecio () 
 {
+ 	var cantidadLamparas;
+ 	var precioLamparasUnidad;
+ 	var costoLamparas;
+ 	var mensaje;
+ 	var marcaLampara;
+ 	var costoFinalLamparas;
+ 	var impuestoE;
  	
+ 	cantidadLamparas = document.getElementById('txtIdCantidad').value;
+ 	cantidadLamparas = parseInt(cantidadLamparas);
+    costoFinalLamparas = document.getElementById('txtIdprecioDescuento').value;
+    precioLamparasUnidad = 35;
+    marcaLampara = document.getElementById('Marca').value;
+    
+    costoLamparas = (precioLamparasUnidad * cantidadLamparas);
+        
+    mensaje = "Usted pagará $";  
+   
+   if(cantidadLamparas>5) 
+     {
+      document.getElementById('txtIdprecioDescuento').value= (mensaje + costoLamparas/2);
+     }
+     else
+    	 {if(cantidadLamparas==5 && marcaLampara=="ArgentinaLuz")
+           {
+           	document.getElementById('txtIdprecioDescuento').value = (mensaje + (costoLamparas-costoLamparas*40/100));
+           }
+             else
+               {
+               	if(cantidadLamparas==5)
+               {
+               	document.getElementById('txtIdprecioDescuento').value = (mensaje + (costoLamparas-costoLamparas*30/100));
+               }
+                 else
+                  {
+                  	if(cantidadLamparas==4 && (marcaLampara=="ArgentinaLuz" || marcaLampara=="FelipeLamparas"))
+                   {
+                    document.getElementById('txtIdprecioDescuento').value = (mensaje + (costoLamparas-costoLamparas*25/100));
+                   }
+                    else
+                     {
+                      if(cantidadLamparas==4)
+                     {
+                      document.getElementById('txtIdprecioDescuento').value = (mensaje + (costoLamparas-costoLamparas*20/100));
+                     }
+                      else
+                      {
+                      	if(cantidadLamparas==3 && marcaLampara=="ArgentinaLuz")
+                      	{
+                      	 document.getElementById('txtIdprecioDescuento').value = (mensaje + (costoLamparas-costoLamparas*15/100));
+                      	}
+                           else
+                        	 {if(marcaLampara=="FelipeLamparas")
+                               {
+                             document.getElementById('txtIdprecioDescuento').value = (mensaje + (costoLamparas-costoLamparas*10/100));
+                               }
+                              else{
+                             	   if(cantidadLamparas==3)
+                             	   {
+                             	  	document.getElementById('txtIdprecioDescuento').value = (mensaje + (costoLamparas-costoLamparas*5/100));
+                             	   }
+                                  }
+                              }
+                           } 
+                        }
+                      }
+                   } 
+               }
+        	
+ 		
+
 }
